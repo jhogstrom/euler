64193c92da84a2f5ab86618a1877a8f13d30d27a
jhogstrom <jhm@SELT00137.tobii.intra>
2012-09-26 07:35:08 +0200

    Solver p 15 kind of.

---
+++ Euler/ListExtender.cs
@@ -0,0 +1,19 @@
+﻿namespace Euler
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class ListExtender
+    {
+        public static IEnumerable<long> Expand(this List<long> me)
+        {
+            yield return me[0];
+            for (int i = 0; i < me.Count - 1; i++)
+            {
+                yield return me[i] + me[i + 1];
+            }
+
+            yield return me.Last();
+        }
+    }
+}
---
+++ Euler/Problem15.cs
@@ -0,0 +1,33 @@
+﻿namespace Euler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Problem15 : EulerProblem
+    {
+        public Problem15(Printing printing)
+            : base(printing)
+        {
+        }
+
+        protected override long GetCalculationResult()
+        {
+            var l = new List<long> { 1, 1 };
+            var c = 1;
+            while (c < 42)
+            {
+                l = l.Expand().ToList();
+                c++;
+                if (c % 2 == 0)
+                {
+                    Console.Write("{0} - ", c / 2);
+                    //l.ForEach(i => Console.Write("{0} ", i));
+                    Console.Write(" -- {0}", l.Max());
+                    Console.WriteLine();
+                }
+            }
+            return 0;
+        }
+    }
+}
