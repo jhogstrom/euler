de9c4129db8d03e43e4384079329ddc9bb34f6e9
jesper hogstrom (jhm) <jesper.hogstrom@tobii.com>
2014-02-28 00:26:11 +0100

    Solved p44 really badly!

--- python/p44.py
+++ python/p44.py
@@ -3,13 +3,28 @@ import math
 def pent(n):
 	return n * ( 3 * n - 1 ) / 2
 
-pents = dict([(i,pent(i)) for i in range(1, 100) ])
+def ispent(n):
+	f = (.5 + math.sqrt(.25+6*n))/3
+   	return f - int(f) == 0
 
+i = 2
+while True:
+	pi = pent(i)
+	j = 1
+	while j < i:
+		pj = pent(j)
+		if ispent(pi-pj) and ispent(pi+pj): 
+			print "Answer:", pi-pj
+			exit()
+		j += 1
+	i += 1
+
+pents = dict([(i,pent(i)) for i in range(1, 10000) ])
 i = 0
 for p in itertools.permutations(pents.items(),2):
-	print p
+	# print p
 	i += 1
 	if abs(p[0][1] - p[1][1]) in pents.values() and p[0][1] + p[1][1] in pents.values():
 		print "diff: ", p, abs(p[0][1] - p[1][1])
-	if not i % 1000:
-		print i
+	# if not i % 1000:
+	# 	print i
